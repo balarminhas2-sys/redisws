8a8da0346c2aadb276385477012206d3f850f528
Balar08 <balarminhas2@gmail.com>
2026-01-07 19:17:22 -0500

    added redis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+import redis
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,3 +12,22 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/name")
+async def name():
+    return {"name":"Balar"}
+
+@app.get("/names/{myname}")
+async def names(myname:str):
+    return {"name":f"your name is {myname}"}
+
+
+
+@app.get("/redis/{key}")
+async def getdata(key:str):
+    r = redis.from_url("rediss://red-d566ok95pdvs73ch3qkg:vFJ6IyTkoE8TUCnwnWLwc9BvEznPaLDU@ohio-keyvalue.render.com:6379")
+    r.set("Temperature", "30")
+    r.set("Humidity", "21")
+    data = r.get(key).decode()
+    return {"key_name":key, "value":data}
