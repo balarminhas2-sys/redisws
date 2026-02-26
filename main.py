640026be71cbcd8140c6a6fa9d3be266987887c6
Balar08 <balarminhas2@gmail.com>
2026-02-25 19:28:05 -0500

    added post option

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from typing import Optional
 import redis
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+r = redis.from_url("rediss://red-d566ok95pdvs73ch3qkg:vFJ6IyTkoE8TUCnwnWLwc9BvEznPaLDU@ohio-keyvalue.render.com:6379")
 
+class weather_data(BaseModel):
+    temperature: float | None = None
+    humidity: float | None = None
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
 
 @app.get("/")
 async def root():
@@ -27,7 +37,25 @@ async def names(myname:str):
 @app.get("/redis/{key}")
 async def getdata(key:str):
     r = redis.from_url("rediss://red-d566ok95pdvs73ch3qkg:vFJ6IyTkoE8TUCnwnWLwc9BvEznPaLDU@ohio-keyvalue.render.com:6379")
-    r.set("Temperature", "30")
-    r.set("Humidity", "21")
     data = r.get(key).decode()
     return {"key_name":key, "value":data}
+
+@app.post("/weather_data")
+async def create_item(item: weather_data):
+    print(item.temperature)
+    print(item.humidity)
+    r.set("Temperature", str(item.temperature))
+    r.set("Humidity", str(item.humidity))
+    return item
+
+@app.post("/weather_data/temperature")
+async def create_temperature(item: weather_data):
+    print(item.temperature)
+    r.set("Temperature", str(item.temperature))
+    return item
+
+@app.post("/weather_data/humidity")
+async def create_humidity(item: weather_data):
+    print(item.humidity)
+    r.set("Humidity", str(item.humidity))
+    return item
